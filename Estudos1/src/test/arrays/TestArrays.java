5e1fb70313f745b1739bc9ccf870816c9ff329a2
sea <sea@sea-virtual-machine>
2018-09-10 19:59:18 -0300

    Testes de arrays

--- Estudos1/src/test/arrays/TestArrays.java
+++ Estudos1/src/test/arrays/TestArrays.java
@@ -3,8 +3,11 @@ package test.arrays;
 class TestArrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] testArray = {1,2,3,4,5}; //ok
+		int testArrayDois[] = {3,2,1}; //ok mas não aconselhavel
+		
+		System.out.println(testArray[4]);
 	}
 
 }
