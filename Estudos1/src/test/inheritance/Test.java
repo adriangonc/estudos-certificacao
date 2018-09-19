140e8fc224c2f0d60409a7a1c65f6c1253016d3a
sea <sea@sea-virtual-machine>
2018-09-19 20:47:56 -0300

    reste

--- Estudos1/src/test/inheritance/Test.java
+++ Estudos1/src/test/inheritance/Test.java
@@ -7,6 +7,6 @@ public class Test {
 	public static void main(String...strings ) {
 		System.out.println(new SubClass1().myObject);
 		System.out.println(a instanceof Interface1); //Imprime true
-		System.out.print(a instanceof Object); //Imprime true
+		System.out.print(a instanceof Object);  //Imprime true
 	}
 }
